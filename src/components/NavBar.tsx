511e670bb9172728d05c1679270d37a712f28e56
Anindra Karmakar <anindrakarmakar+codeberg.org@proton.me>
2025-05-18 18:17:21 +0530

    Fix color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
 
   return (
     <div>
-      <nav className="bg-gray-800">
+      <nav className="bg-slate-900">
         <div className="mx-auto max-w-full px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
